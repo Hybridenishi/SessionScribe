50f12d3dff29ab8dffc608a4ffc3fdf05dc50006
Nathaniel Davis <karasutakai@gmail.com>
2026-07-21 14:16:49 -0400

    Initial Commit

---
+++ SessionScribe/SessionScribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SessionScribeApp.swift
+//  SessionScribe
+//
+//  Created by Nate Davis on 7/21/26.
+//
+
+import SwiftUI
+
+@main
+struct SessionScribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SessionScribeTests/SessionScribeTests.swift
@@ -0,0 +1,19 @@
+//
+//  SessionScribeTests.swift
+//  SessionScribeTests
+//
+//  Created by Nate Davis on 7/21/26.
+//
+
+import Testing
+@testable import SessionScribe
+
+struct SessionScribeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
